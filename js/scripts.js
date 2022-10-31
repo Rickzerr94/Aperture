b56d54a4478b86b5bf6937f80004b7ab98bf048e
Rickzerr94 <rickzerr91@gmail.com>
2022-10-31 21:41:35 +0200

    add owl carousel, cannot add owl-dots

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
 $( function() {
     $( "#accordion" ).accordion();
 } );
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    navText: [ '', ' ' ],
+    autoplay: false,
+    autoplayTimeout:2000,
+    responsive:{
+        0:{
+            items:2
+        },
+
+        1000:{
+            items:5
+        }
+    }
+});
